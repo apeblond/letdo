a9081a1cd4c1c39a6c18d6c80cfc0223bb2ba87a
Dave Apeblond <109555139+apeblond@users.noreply.github.com>
2022-11-17 18:40:46 -0300

    Add Factory contract

---
+++ src/contracts/LetdoStoreFactory.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import "./LetdoStore.sol";
+
+contract LetdoStoreFactory {
+    address[] public allStores;
+
+    event StoreCreated(address store, address indexed owner);
+
+    address public storesCurrencyERC20;
+
+    error EmptyString();
+
+    constructor(address _storesCurrencyERC20) {
+        require(_storesCurrencyERC20 != address(0));
+        storesCurrencyERC20 = _storesCurrencyERC20;
+    }
+
+    function allStoresLength() external view returns (uint256) {
+        return allStores.length;
+    }
+
+    function createStore(string calldata storeName) external returns (address) {
+        if (bytes(storeName).length == 0) revert EmptyString();
+
+        bytes32 salt = keccak256(
+            abi.encodePacked(allStores.length, msg.sender)
+        );
+
+        address newStoreAddress = address(
+            new LetdoStore{salt: salt}(storeName, storesCurrencyERC20)
+        );
+
+        allStores.push(newStoreAddress);
+
+        emit StoreCreated(newStoreAddress, msg.sender);
+
+        return newStoreAddress;
+    }
+}
---
+++ test/LetdoStoreFactory.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/contracts/LetdoStoreFactory.sol";
+import "./TestERC20.sol";
+
+contract LetdoStoreTest is Test {
+    address public storeOwner = address(1);
+    LetdoStoreFactory public factory;
+
+    function setUp() public {
+        vm.startPrank(storeOwner);
+        factory = new LetdoStoreFactory(address(new TestERC20()));
+        vm.stopPrank();
+    }
+
+    function testStoreCreation() public {
+        assertEq(factory.allStoresLength(), 0);
+        address createdStore = factory.createStore("Test");
+        assertFalse(createdStore == address(0));
+        assertEq(factory.allStoresLength(), 1);
+        assertEq(factory.allStores(0), createdStore);
+    }
+
+    function testCreateMultipleStores() public {
+        testStoreCreation();
+        address createdStore = factory.createStore("Test");
+        assertEq(factory.allStoresLength(), 2);
+        assertEq(factory.allStores(1), createdStore);
+    }
+
+    function testFailStoreCreationWithEmptyString() public {
+        assertEq(factory.allStoresLength(), 0);
+        factory.createStore("");
+    }
+}
