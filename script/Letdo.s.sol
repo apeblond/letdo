48102ab5f6287df3549fad825f3b3197efbe6563
Dave Apeblond <109555139+apeblond@users.noreply.github.com>
2022-11-19 21:21:38 -0300

    Add deployment script

--- script/Letdo.s.sol
+++ script/Letdo.s.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/contracts/LetdoStoreFactory.sol";
 
 contract LetdoScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        new LetdoStoreFactory(0x9D233A907E065855D2A9c7d4B552ea27fB2E5a36); // Deploying it with Goerli DAI
+        vm.stopBroadcast();
     }
 }
